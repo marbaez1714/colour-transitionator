1fda8ed6645d5fab4c9598570cee613b30f3d223
marbaez1714 <marbaez1714@gmail.com>
2019-07-02 15:42:22 -0500

    gh-pages

---
+++ build/precache-manifest.52d96c393ac9e915118ce1adb7f96c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e553e727e258d25b78907f465d0fb9ec",
+    "url": "/colour-transitionator/index.html"
+  },
+  {
+    "revision": "643ef67f1c5c85dd635a",
+    "url": "/colour-transitionator/static/css/main.0853ebe8.chunk.css"
+  },
+  {
+    "revision": "1c593fae14513dcd4243",
+    "url": "/colour-transitionator/static/js/2.a214b9dc.chunk.js"
+  },
+  {
+    "revision": "643ef67f1c5c85dd635a",
+    "url": "/colour-transitionator/static/js/main.1eaaf6ef.chunk.js"
+  },
+  {
+    "revision": "04c455359d6a8aacf976",
+    "url": "/colour-transitionator/static/js/runtime~main.d8142613.js"
+  }
+]);
